b73f5b9f5cb8ae282418887af6c1856db9edbde8
prakash1895 <baskarap@oregonstate.edu>
2020-05-09 03:23:21 -0500

    Finding correspondence & MgW calculations are completed. Analytical solution part is left

--- scan-matching-lab/nautilus_scan_matching/src/scan_matching.cpp
+++ scan-matching-lab/nautilus_scan_matching/src/scan_matching.cpp
@@ -20,9 +20,16 @@
 #include <eigen_conversions/eigen_msg.h>
 #include <tf_conversions/tf_eigen.h>
 #include <tf/transform_datatypes.h>
+
 #include "nautilus_scan_matching/JumpTable.h"
+#include "nautilus_scan_matching/Correspondence.h"
 
 using namespace std;
+
+inline double SQUARE(double s) 
+{ 
+    return s*s; 
+} 
 
 class ScanMatching 
 {
@@ -40,13 +47,14 @@ class ScanMatching
     sensor_msgs::LaserScan prev_scan_;
     geometry_msgs::PoseStamped estimated_pose_;
     
-    //Topic names for publishers and subscribers
+    //Topic names & CONST
     const string& SCAN_TOPIC  = "/scan";
     const string& POSE_TOPIC = "/scan_match_pose";
     const string& ODOM_TOPIC = "/odom";
     const string& FAKE_SCAN_TOPIC = "/fake_scan_match";
     const string& DRIVE_TOPIC = "/nav";
-    const int MAX_ITERATIONS = 100;
+    const int MAX_ITERATIONS = 30;
+    const double MAX_CORRESPONDENCE_DIST = 3.0;
 
     //Publishers
     ros::Publisher pose_pub_;
@@ -90,30 +98,30 @@ class ScanMatching
         tf::transformEigenToTF(map_laser_eigen, tr_);
     }
 
-    nautilus_scan_matching::JumpTable update_jump_table(const sensor_msgs::LaserScan prev_scan)
+    nautilus_scan_matching::JumpTable update_jump_table(const sensor_msgs::LaserScan scan_msg)
     {
         int i,j;
-        nautilus_scan_matching::JumpTable prev_scan_jt;
+        nautilus_scan_matching::JumpTable scan_jt;
 
-        for(i=0; i<prev_scan.ranges.size(); i++)
+        for(i=0; i<scan_msg.ranges.size(); i++)
         {
             j = i+1;
-            while(j<prev_scan.ranges.size() && prev_scan.ranges[j] <= prev_scan.ranges[i]) j++;
-            prev_scan_jt.up_bigger.push_back(j-i);
+            while(j<scan_msg.ranges.size() && scan_msg.ranges[j] <= scan_msg.ranges[i]) j++;
+            scan_jt.up_bigger.push_back(j-i);
 
             j = i+1;
-            while(j<prev_scan.ranges.size() && prev_scan.ranges[j] >= prev_scan.ranges[i]) j++;
-            prev_scan_jt.up_smaller.push_back(j-i);
+            while(j<scan_msg.ranges.size() && scan_msg.ranges[j] >= scan_msg.ranges[i]) j++;
+            scan_jt.up_smaller.push_back(j-i);
 
             j = i-1;
-            while(j>=0 && prev_scan.ranges[j] <= prev_scan.ranges[i]) j--;
-            prev_scan_jt.down_bigger.push_back(j-i);
+            while(j>=0 && scan_msg.ranges[j] <= scan_msg.ranges[i]) j--;
+            scan_jt.down_bigger.push_back(j-i);
 
             j = i-1;
-            while(j>=0 && prev_scan.ranges[j] >= prev_scan.ranges[i]) j--;
-            prev_scan_jt.down_smaller.push_back(j-i);            
+            while(j>=0 && scan_msg.ranges[j] >= scan_msg.ranges[i]) j--;
+            scan_jt.down_smaller.push_back(j-i);            
         }
-        return prev_scan_jt;
+        return scan_jt;
     }
 
     std::vector<geometry_msgs::Point> convert_LaserScan_toPCL(const sensor_msgs::LaserScan scan_msg)
@@ -131,6 +139,12 @@ class ScanMatching
             points_.push_back(temp_pt);
         }
         return points_;
+    }
+
+    tf::Transform update_transform(tf::Transform tr)
+    {
+        tf::Transform new_tr = tr;
+        return tr;
     }
 
     std::vector<geometry_msgs::Point> get_roto_translation(const std::vector<geometry_msgs::Point> points, const tf::Transform tr)
@@ -151,31 +165,269 @@ class ScanMatching
         return trans_points_;
     }
 
+    void Cartesian_toPolar(const geometry_msgs::Point tp, double &norm, double &theta)
+    {
+        norm = sqrt(tp.x*tp.x + tp.y*tp.y);
+        theta = atan2(tp.y, tp.x);
+    }
+
+    double calculateDistance(const geometry_msgs::Point P_iw, const geometry_msgs::Point P_up)
+    {
+        double x = P_iw.x - P_up.x;
+        double y = P_iw.y - P_up.y;
+        return (x*x + y*y);
+    }
+
+    std::vector<nautilus_scan_matching::Correspondence> findCorrespondence(nautilus_scan_matching::JumpTable prev_scan_jt)
+    {
+        nautilus_scan_matching::Correspondence temp_corr;
+        std::vector<nautilus_scan_matching::Correspondence> C_k;
+    
+        const int MIN_IDX = 0;
+        const int MAX_IDX = transformed_points_.size();
+
+        int last_best = -1;
+        double C = (double)prev_scan_.ranges.size()/(prev_scan_.angle_max - prev_scan_.angle_min);
+        
+        for(int i=0; i<transformed_points_.size(); i++)
+        {
+            double p_iw_norm, p_iw_phi;
+            Cartesian_toPolar(transformed_points_[i], p_iw_norm, p_iw_phi);
+            int start_cell = (int)((p_iw_phi - prev_scan_.angle_min)*C);
+
+            int j1 = -1;
+            double best_dist = 2*prev_scan_.range_max;
+            int we_start_at;
+            if (last_best == -1)
+                we_start_at = start_cell;
+            else
+                we_start_at = last_best + 1;
+
+            if (we_start_at > MAX_IDX) we_start_at = MAX_IDX;
+            if (we_start_at < MIN_IDX) we_start_at = MIN_IDX;
+
+            int up = we_start_at+1;
+            int down = we_start_at;
+            double last_dist_up = 0.00; //doubts set to inf;
+            double last_dist_down = -1.00; // same doubts;
+
+            bool up_stopped = false;
+            bool down_stopped = false;
+
+            //Optional print statement
+
+            while( (!up_stopped) || (!down_stopped))
+            {
+                // int now_up = up_stopped ? 0 : down_stopped ? 1 : last_dist_up < last_dist_down;
+
+                bool now_up;
+                if (up_stopped){ now_up = false; }
+                else if (down_stopped) { now_up = true; }
+                else { now_up = (last_dist_up < last_dist_down); }
+                
+                if (now_up)
+                {
+                    if (up > MAX_IDX){up_stopped = true; continue;}
+
+                    last_dist_up = calculateDistance(transformed_points_[i], prev_points_[up]);
+                    if ((last_dist_up < best_dist) || (j1 ==-1)){ j1 = up; best_dist = last_dist_up;}
+                    
+                    if (up > start_cell)
+                    {
+                        double up_phi = prev_scan_.angle_min + up*prev_scan_.angle_increment;
+                        double del_phi = up_phi - p_iw_phi;
+                        double min_dist_up = sin(del_phi)*p_iw_norm;
+                        
+                        if (SQUARE(min_dist_up) > best_dist){up_stopped = true; continue;}
+
+                        if (prev_scan_.ranges[up] < p_iw_norm)
+                            up += prev_scan_jt.up_bigger[up];
+                        else
+                            up += prev_scan_jt.up_smaller[up];   
+                    }
+                    else
+                        up++;
+                }
+
+                if (!now_up)
+                {
+                    if (down < MIN_IDX){down_stopped = true; continue;}
+
+                    last_dist_down = calculateDistance(transformed_points_[i], prev_points_[down]);
+                    if ((last_dist_down < best_dist) || (j1 == -1)){ j1 = down; best_dist = last_dist_down; }
+
+                    if (down < start_cell)
+                    {
+
+                        double down_phi = prev_scan_.angle_min + down*prev_scan_.angle_increment;
+                        double del_phi = p_iw_phi -  down_phi;
+                        double min_dist_down = sin(del_phi)*p_iw_norm;
+                        
+                        if (SQUARE(min_dist_down) > best_dist){down_stopped = true; continue;}
+
+                        if (prev_scan_.ranges[down] < p_iw_norm)
+                            down += prev_scan_jt.down_bigger[down];
+                        else
+                            down += prev_scan_jt.down_smaller[down]; 
+                    }
+                    else
+                        down--;
+                }
+            }
+
+            // outside while
+            //if no match point found & accounting for boundary condns. 
+            if ((j1 <= MIN_IDX) || (j1 >= MAX_IDX) || (j1 == -1) || (best_dist > MAX_CORRESPONDENCE_DIST))
+            {
+                temp_corr.valid = false;
+                temp_corr.j1 = -1;
+                temp_corr.j2 = -1;
+                temp_corr.dist_j1 = 0.00;
+                C_k.push_back(temp_corr);
+                continue;
+            }
+
+            int j2;
+            double j2_dist;
+
+            int j2_up = j1 + 1;
+            if (j2_up < MIN_IDX || j2_up > MAX_IDX)
+                j2_up = -1;
+
+            int j2_down = j1 - 1;
+            if (j2_down < MIN_IDX || j2_down > MAX_IDX)
+                j2_down = -1;
+
+            if ((j2_up == -1) && (j2_down == -1))
+            {
+                temp_corr.valid = false;
+                temp_corr.j1 = -1;
+                temp_corr.j2 = -1;
+                temp_corr.dist_j1 = 0.00;
+                C_k.push_back(temp_corr);
+                continue;
+            }
+
+            // Decide j2 up/down 
+            if(j2_up == -1) 
+                j2 = j2_down;
+
+            else if(j2_down == -1) 
+                j2 = j2_up;
+
+            else 
+            {
+                double j2_up_dist = calculateDistance(transformed_points_[i], prev_points_[j2_up]);
+                double j2_down_dist = calculateDistance(transformed_points_[i], prev_points_[j2_down]);
+                if (j2_up_dist < j2_down_dist)
+                {
+                    j2 = j2_up;
+                    j2_dist = j2_up_dist;
+                }
+
+                else
+                {
+                    j2 = j2_down;
+                    j2_dist = j2_down_dist;
+                }
+            }
+
+            temp_corr.valid = true;
+            temp_corr.j1 = j1;
+            temp_corr.j2 = j2;
+            temp_corr.dist_j1 = best_dist;
+            temp_corr.dist_j2 = j2_dist;
+            C_k.push_back(temp_corr);
+
+            last_best = j1;
+            // cerr<<"p_iw: "<<i<<" j1: "<<j1<<" j2: "<<j2<<" dist_j1: "<<best_dist<<" dist_j2: "<<j2_dist<<endl; 
+        }
+
+        return C_k;
+    }
+
+    void get_MgW_matrices(const std::vector<nautilus_scan_matching::Correspondence> C_k, Eigen::Matrix4d &M, Eigen::RowVector4d &g, Eigen::Matrix4d &W)
+    {
+        M = Eigen::MatrixXd::Zero(4, 4);
+        g<<0,0,0,0;
+        W<<0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+        Eigen::MatrixXd Mi(2,4);
+        Eigen::Vector2d ni;
+        Eigen::Matrix2d Ci;
+        Eigen::Vector2d PI_i;
+
+        for(int i=0; i<C_k.size(); i++)
+        {   
+            // ignore false match points
+            if(C_k[i].valid == false)
+                continue;
+
+            int j1 = C_k[i].j1;
+            int j2 = C_k[i].j2;
+
+            Mi<<1, 0, curr_points_[i].x, -curr_points_[i].y,
+            0, 1, curr_points_[i].y, curr_points_[i].x;
+
+            double dx = prev_points_[j1].x - prev_points_[j2].x;
+            double dy = prev_points_[j1].y - prev_points_[j2].y;
+            ni<<dy, -dx;
+            ni.normalize();
+            Ci = ni*ni.transpose();
+
+            PI_i<<prev_points_[j1].x, prev_points_[j1].y;
+
+            Eigen::Matrix4d temp_M = Mi.transpose()*Ci*Mi;
+            Eigen::RowVector4d temp_g = -2.0*PI_i.transpose()*Ci*Mi;
+
+            M += temp_M;
+            g += temp_g;
+        }
+
+    }
+
     void do_scan_matching(const sensor_msgs::LaserScan curr_scan_) 
     {
         //The following steps are inspired from the Censi's PL-ICP paper. Try to modularize your code for each step to make it more readable and debuggable.
         //Use Eigen math library for linear algebra, matrix and vector operations, geometrical transformations.
-        
-        /*1.Compute the coordinates of the second scan’s points in the first scan’s frame of reference, 
-        according to the roto-translation obtained from odometry update.*/
-        curr_points_ = convert_LaserScan_toPCL(curr_scan_);
-        transformed_points_ = get_roto_translation(curr_points_, tr_);
-        
-        /*2.Find correspondence between points of the current and previous frame. You can use naive way of looking 
-        through all points in sequence or use radial ordering of laser points to speed up the search.*/
-        nautilus_scan_matching::JumpTable prev_scan_jt = update_jump_table(prev_scan_);
 
-        
-        //3. Based on the correspondences, find the necessary tranform.
+        // obtain curr_points_, calculate jump table & set up initial guess q0
+        curr_points_ = convert_LaserScan_toPCL(curr_scan_);
+        nautilus_scan_matching::JumpTable prev_scan_jt = update_jump_table(prev_scan_);
+        tf::Transform cur_tr = update_transform(tr_);
+
+        for(int k=0; k<MAX_ITERATIONS; k++)
+        {
+            /*1.Compute the coordinates of the second scan’s points in the first scan’s frame of reference, 
+            according to the roto-translation obtained from odometry update.*/
+            transformed_points_ = get_roto_translation(curr_points_, cur_tr);
+            
+            /*2.Find correspondence between points of the current and previous frame. You can use naive way of looking 
+            through all points in sequence or use radial ordering of laser points to speed up the search.*/
+            std::vector<nautilus_scan_matching::Correspondence> C_k;
+            C_k = findCorrespondence(prev_scan_jt);
+
+            //3. Based on the correspondences, find the necessary tranform.
             //3.a. Construct the necessary matrices as shown in the paper for solution with Lagrange's multipliers.
 
+            Eigen::Matrix4d M;
+            Eigen::Matrix4d W;
+            Eigen::RowVector4d g;
+            get_MgW_matrices(C_k, M, g, W);
+
+            Eigen::Vector4d X;
+            
             //3.b. You should get a fourth order polynomial in lamda which you can solve to get value(hint:greatest real root of polynomial equn) of lamda.
 
-            //3.b. Use the calcualted value of lamda to estimate the transform using equation 24 in the Censi's paper.
+            //3.b. Use the calculated value of lamda to estimate the transform using equation 24 in the Censi's paper.
 
+            //update cur_tr based on X output
+        }
 
         //4.Publish the estimated pose from scan matching based on the transform obstained. You can visualize the pose in rviz.
-
 
         /*5.Also transform the previous frame laserscan points using the roto-translation transform obtained and visualize it. Ideally, this should coincide
         with your actual current laserscan message.*/
