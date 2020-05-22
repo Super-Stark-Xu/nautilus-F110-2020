a970ac3752e3b08982698b212d13cae1c6e3b19c
prakash1895 <baskarap@oregonstate.edu>
2020-05-21 20:02:04 -0500

    Fix the pure_pursuit_sim.launch bug and changed the program structure

--- pure-pursuit-lab/nautilus_pure_pursuit/scripts/pure_pursuit.py
+++ pure-pursuit-lab/nautilus_pure_pursuit/scripts/pure_pursuit.py
@@ -10,72 +10,61 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import csv
 import os 
 
-#############
-# CONSTANTS #
-#############
+class PurePursuit:
+    def __init__(self, filepath="DEFAULT"):
+        self.LOOKAHEAD_DISTANCE = 2.0 #meters
+        self.VELOCITY = 1.0 #m/s
 
-LOOKAHEAD_DISTANCE = 2.0 # meters
-VELOCITY = 1.0 # m/s
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, '../waypoints/levine-waypoints.csv')
+        with open(filename) as f:
+            path_points = [tuple(line) for line in csv.reader(f)]
 
+        # Turn path_points into a list of floats to eliminate the need for casts in the code below.
+        self.path_points = [(float(point[0]), float(point[1]), float(point[2])) for point in path_points]
+                
+        # Publisher for 'drive_parameters' (speed and steering angle)
+        self.pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
 
-###########
-# GLOBALS #
-###########
+        # Subscriber for odom
+        self.sub = rospy.Subscriber('/odom', Odometry, self.callback, queue_size=1)
 
-# Import waypoints.csv into a list (path_points)
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, '../waypoints/levine-waypoints.csv')
-with open(filename) as f:
-    path_points = [tuple(line) for line in csv.reader(f)]
+    # Computes the Euclidean distance between two 2D points p1 and p2.
+    def dist(self, p1, p2):
+        return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
-# Turn path_points into a list of floats to eliminate the need for casts in the code below.
-path_points = [(float(point[0]), float(point[1]), float(point[2])) for point in path_points]
-        
-# Publisher for 'drive_parameters' (speed and steering angle)
-pub = rospy.Publisher('drive_parameters', drive_param, queue_size=1)
+    # Input data is PoseStamped message from topic /odom.
+    # Runs pure pursuit and publishes velocity and steering angle.
+    def callback(self, msg):
 
+        # Note: These following numbered steps below are taken from R. Craig Coulter's paper on pure pursuit.
 
-#############
-# FUNCTIONS #
-#############
-    
-# Computes the Euclidean distance between two 2D points p1 and p2.
-def dist(p1, p2):
-    return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
-
-# Input data is PoseStamped message from topic /odom.
-# Runs pure pursuit and publishes velocity and steering angle.
-def callback(msg):
-
-    # Note: These following numbered steps below are taken from R. Craig Coulter's paper on pure pursuit.
-
-    # 1. Determine the current location of the vehicle (we are subscribed to /odom)
-    # Hint: Read up on Odometry message type in ROS to determine how to extract x, y, and yaw. Make sure to convert quaternion to euler angle.
-    
-
+        # 1. Determine the current location of the vehicle (we are subscribed to /odom)
+        # Hint: Read up on Odometry message type in ROS to determine how to extract x, y, and yaw. Make sure to convert quaternion to euler angle.
         
 
-    # 2. Find the path point closest to the vehicle that is >= 1 lookahead distance from vehicle's current location.
+        # 2. Find the path point closest to the vehicle that is >= 1 lookahead distance from vehicle's current location.
 
 
+        # 3. Transform the goal point to vehicle coordinates. 
+        
+        
+        # 4. Calculate the curvature = 1/r = 2x/l^2
+        # The curvature is transformed into steering wheel angle and published to the 'drive_param' topic.
 
-    # 3. Transform the goal point to vehicle coordinates. 
-    
-    
+        angle = np.clip(angle, -0.4189, 0.4189) # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
 
-    # 4. Calculate the curvature = 1/r = 2x/l^2
-    # The curvature is transformed into steering wheel angle and published to the 'drive_param' topic.
-
-    
-    angle = np.clip(angle, -0.4189, 0.4189) # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
-
-    msg = drive_param()
-    msg.velocity = VELOCITY
-    msg.angle = angle
-    pub.publish(msg)
+        msg = drive_param()
+        msg.velocity = self.VELOCITY
+        msg.angle = angle
+        self.pub.publish(msg)
     
 if __name__ == '__main__':
     rospy.init_node('pure_pursuit')
-    rospy.Subscriber('/odom', Odometry, callback, queue_size=1)
-    rospy.spin()
+    pp_obj = PurePursuit()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down PurePursuit...")
+
 
