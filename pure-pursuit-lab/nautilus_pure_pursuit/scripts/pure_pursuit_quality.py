ad0f1fce46e285cd8d0183c24d5bc87586aba7c6
Live session user <hugheste@oregonstate.edu>
2020-05-22 02:19:19 +0000

    Added pure pursuit quality file to calculate error

---
+++ pure-pursuit-lab/nautilus_pure_pursuit/scripts/pure_pursuit_quality.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+import rospy
+#from race.msg import drive_param
+from nav_msgs.msg import Odometry
+import math
+import numpy as np
+from numpy import linalg as LA
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import csv
+import os
+from nav_msgs.msg import Path
+from csv import writer
+
+#############
+# CONSTANTS #
+#############
+
+WINDOW_SIZE = 100
+
+###########
+# GLOBALS #
+###########
+
+# Import waypoints.csv into a list (path_points)
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../waypoints/levine-waypoints.csv')
+with open(filename) as f:
+    path_points = [tuple(line) for line in csv.reader(f)]
+
+# Turn path_points into a list of floats to eliminate the need for casts in the code below.
+path_points = [(float(point[0]), float(point[1]), float(point[2])) for point in path_points]
+path_idx = 0
+num_points = 0
+total_abs_max = 0
+error = np.array([])
+max_errors = np.array([])
+
+# Publisher for 'drive_parameters' (speed and steering angle)
+#pub = rospy.Publisher('pursuit_quality', float, queue_size=1)
+
+#############
+# FUNCTIONS #
+#############
+
+# Computes the Euclidean distance between two 2D points p1 and p2.
+def dist(p1, p2):
+    return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+def append_to_csv(filename, error_data):
+    # Open file in append mode
+    with open(filename, "a") as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add error values as last row in the csv file
+        csv_writer.writerow(error_data)
+
+# Input data is PoseStamped message from topic /odom.
+# Runs pure pursuit and publishes velocity and steering angle.
+def callback(msg):
+    global path_points
+    global path_idx
+    global num_points
+    global error
+    global max_errors
+    global total_abs_max
+
+    actual_point = [msg.pose.pose.position.x,msg.pose.pose.position.y]
+    desired_point = path_points[path_idx]
+    num_points += 1
+
+    #print("actual_point: ",actual_point)
+    #print("desired_point: ",desired_point)
+
+    if path_idx >= len(path_points):
+        path_idx = 0
+    else:
+        path_idx += 1
+
+    error = np.append(error,dist(actual_point, desired_point))
+
+    if (num_points % WINDOW_SIZE) == 0:
+        print("**** After window at: ",num_points," *****")
+        abs_max = np.max(error)
+        print("abs_max: ",abs_max)
+        max_errors = np.append(max_errors,abs_max)
+        error = np.array([])
+
+        total_abs_max += np.max(max_errors)
+        print("total_abs_max: ",total_abs_max)
+
+        append_to_csv("error_values.csv", [abs_max,total_abs_max])
+	#df = pd.DataFrame(self.points_list, columns=['x', 'y', 'theta'])
+	#df.to_csv('waypoints_saver.csv')
+
+    #orientation = msg.pose.pose.orientation
+    #msg = drive_param()
+    #msg.velocity = VELOCITY
+    #msg.angle = angle
+    #pub.publish(msg)
+
+if __name__ == '__main__':
+    rospy.init_node('pure_pursuit_quality')
+    rospy.Subscriber('/odom', Odometry, callback, queue_size=1)
+    rospy.spin()
