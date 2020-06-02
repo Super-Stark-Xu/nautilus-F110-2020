bb3909d66bf1078d2e8e3e9b79243c188eeec47c
Super-Stark-Xu <63092237+Super-Stark-Xu@users.noreply.github.com>
2020-06-02 05:04:49 -0700

    max_speed on straight line C2 C3, with breaking issue

--- final-race/nautilus_final_race/scripts/nautilus_race.py
+++ final-race/nautilus_final_race/scripts/nautilus_race.py
@@ -20,7 +20,7 @@ import os
 import time
 
 MAX_VEL = 6.5
-MIN_VEL = 1.0
+MIN_VEL = 2.0
 L_CONST = 5.0
 
 class FinalRace:
@@ -42,12 +42,12 @@ class FinalRace:
         self.text_marker_pub = rospy.Publisher('display_param', Marker, queue_size=1)
 
         self.idx = 0
-        self.chk_idx = [0, 700, 1750, 2300, 3000, 3700]
+        self.chk_idx = [0, 150, 700, 1665, 2300, 3000, 3700, 4100]#-,1750,-
         self.m = (MIN_VEL - MAX_VEL)/24.0
         self.c = MAX_VEL
         self.LOOKAHEAD_DISTANCE = 2.0 #meters
         self.initialpose_publish()
-        self.key_publish()
+#        self.key_publish()
 
         # Subscriber for odom
         self.reset_sub = rospy.Subscriber('/reset_car', Bool, self.reset_callback, queue_size=1 )
@@ -88,7 +88,7 @@ class FinalRace:
         text_marker.scale.y = 1.0
         text_marker.scale.z = 1.0
         text_marker.color.a = 1.0
-        text_marker.color.r = 1.0    
+        text_marker.color.r = 0.0    
         text_marker.pose.position.x = -6.388 # cur_pos[0]
         text_marker.pose.position.y = -9.032 # cur_pos[1]
         text_marker.pose.position.z = 0.0
@@ -96,8 +96,8 @@ class FinalRace:
         text_marker.pose.orientation.y = 0.0
         text_marker.pose.orientation.z = 0.0
         text_marker.pose.orientation.w = 1.0
-        ang_deg = str(round(math.degrees(drive_msg.angle), 2))
-        velocity = str(round(drive_msg.velocity, 2))
+        ang_deg = str(round(math.degrees(drive_msg.angle), 1))
+        velocity = str(round(drive_msg.velocity, 1))
         text_msg = "Velocity: " + velocity + "\nSteering: " + ang_deg
         text_marker.text =  text_msg
         self.text_marker_pub.publish(text_marker)
@@ -118,7 +118,7 @@ class FinalRace:
             self.LOOKAHEAD_DISTANCE = 1.0
             
         vel = self.m*abs(ang_deg) + self.c
-        print(abs(ang_deg), vel, self.idx)
+ #       print(abs(ang_deg), vel, self.idx)
         return vel
 
     def TransformPoint(self, desired_point):
@@ -155,10 +155,12 @@ class FinalRace:
                     min_dist = dist_diff
                     desired_point = path_point
                     self.idx = i
-
+#############
+		    velocity_idx = i
+##############
             if (L > L_CONST*self.LOOKAHEAD_DISTANCE): # Prune Search Tree
                 break
-        return desired_point    
+        return desired_point, velocity_idx    
     
     def reset_idx(self, cur_idx):
         close = 1e5
@@ -185,7 +187,7 @@ class FinalRace:
         # new search method
         desired_point = None
         while(desired_point is None):
-            desired_point = self.searchNew(cur_pos, self.path_points)
+            desired_point, velocity_idx = self.searchNew(cur_pos, self.path_points)
             if desired_point == None:
                 rospy.sleep(0.1)
                 self.idx = self.reset_idx(self.idx)
@@ -212,6 +214,15 @@ class FinalRace:
 
         angle = np.clip(angle, -0.4189, 0.4189) # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
         velocity = self.update_lookahead(angle)
+	
+	velocity_tmp = velocity 
+	if velocity_idx > 1664 and velocity_idx < 2200: # max speed on straight line
+		velocity = 10
+		if velocity_idx > 2200 and velocity_idx < 2300: # break before curve
+			velocity = (MAX_VEL - 10)*(velocity_idx - 2200)/100 + MAX_VEL
+
+#	if self.idx > 4099 or self.idx < 150:
+#		velocity = 10
         
         msg = drive_param()
         msg.velocity = velocity
